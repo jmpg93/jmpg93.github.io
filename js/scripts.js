f15dbe6f66ba27e6b4eb7380ae76f658f9d97f38
José María Puerta <jmpg93@gmail.com>
2021-03-03 23:31:41 +0100

    Update script

--- js/scripts.js
+++ js/scripts.js
@@ -194,19 +194,46 @@
             From : 'jmpg932@gmail.com',
             Subject : "Petición de información web.",
             Body : `
-            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN">
-            <html xmlns="http://www.w3.org/1999/xhtml">
-              <head>
+            <html>
+
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             </head>
-            <body style="margin: 2rem; padding: 25; border: 0;" background="#ffffff">
-                <h3></h3>
-                <p>Nombre: ${name}</p>
-                <p>Email: ${email}</p>
-                <p>Teléfono: ${phone}</p>
-                <br>
-                <p>Mensaje:</p>
-                <p>${message}</p>
+            
+            <body class="bg-light">
+                <div class="container">
+                    <div class="card my-5">
+                        <div class="card-body">
+                            <h1>Petición de información desde la web.</h1>
+                            <h5 style="display: inline;" class="text-muted font-weight-bold">Nombre: </h5>
+                            <h5 style="display: inline;" class="text-muted">${name}</h5>
+                            <br>
+                            <h5 style="display: inline;" class="text-muted font-weight-bold">Email: </h5>
+                            <h5 style="display: inline;" class="text-muted">${email}</h5>
+                            <br>
+                            <h5 style="display: inline;" class="text-muted font-weight-bold">Teléfono: </h5>
+                            <h5 style="display: inline;" class="text-muted">${phone}</h5>
+                            <br>
+                            <br>
+                            <h5 class="text-muted font-weight-bold">Mensaje: </h5>
+                            <h5 class="text-muted">${message}</h5>
+                        </div>
+                    </div>
+                </div>
+            
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                    crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+                    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+                    crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+                    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+                    crossorigin="anonymous"></script>
             </body>
+            
             </html>
             `
         }).then(function(message) {
